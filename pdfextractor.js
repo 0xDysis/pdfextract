49d728047e5d8a8b17d82338018079e7fbb05b79
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-11-05 18:07:27 +0100

    Add files via upload

--- pdfextractor.js
+++ pdfextractor.js
@@ -1,8 +1,7 @@
 const Imap = require('imap');
 const simpleParser = require('mailparser').simpleParser;
-const fs = require('fs');
-const path = require('path');
 const nodemailer = require('nodemailer');
+const cron = require('node-cron');
 
 let imap = new Imap({
   user: 'timmy.moreels@gmail.com',
@@ -26,70 +25,75 @@ function openInbox(cb) {
   imap.openBox('INBOX', false, cb);
 }
 
-imap.once('ready', function() {
-  openInbox(function(err, box) {
-    if (err) throw err;
-    imap.search(['UNSEEN', ['FROM', 'dysishomer@gmail.com']], function(err, results) {
+function checkEmails() {
+  imap.once('ready', function() {
+    openInbox(function(err, box) {
       if (err) throw err;
-      if (results.length === 0) {
-        console.log('No new emails to fetch');
-        return;
-      }
-      let f = imap.fetch(results, { bodies: '', markSeen: true });
-      f.on('message', function(msg, seqno) {
-        let rawEmail = '';
-        msg.on('body', function(stream, info) {
-          stream.on('data', function(chunk) {
-            rawEmail += chunk.toString('utf8');
-          });
-          stream.once('end', function() {
-            simpleParser(rawEmail, (err, mail) => {
-              if (err) throw err;
-              if (mail.attachments) {
-                mail.attachments.forEach(function(attachment) {
-                  if (attachment.contentType === 'application/pdf') {
-                    console.log('PDF found in the email'); // Debugging line
-                    let mailOptions = {
-                      from: 'timmy.moreels@gmail.com',
-                      to: 'dysiscypher@gmail.com',
-                      subject: 'Extracted PDF',
-                      text: '',
-                      attachments: [
-                        {
-                          filename: attachment.filename,
-                          content: attachment.content
+      imap.search(['UNSEEN', ['FROM', 'dysishomer@gmail.com']], function(err, results) {
+        if (err) throw err;
+        if (results.length === 0) {
+          console.log('No new emails to fetch');
+          return;
+        }
+        let f = imap.fetch(results, { bodies: '', markSeen: true });
+        f.on('message', function(msg, seqno) {
+          let rawEmail = '';
+          msg.on('body', function(stream, info) {
+            stream.on('data', function(chunk) {
+              rawEmail += chunk.toString('utf8');
+            });
+            stream.once('end', function() {
+              simpleParser(rawEmail, (err, mail) => {
+                if (err) throw err;
+                if (mail.attachments) {
+                  mail.attachments.forEach(function(attachment) {
+                    if (attachment.contentType === 'application/pdf') {
+                      console.log('PDF found in the email'); // Debugging line
+                      let mailOptions = {
+                        from: 'timmy.moreels@gmail.com',
+                        to: 'dysiscypher@gmail.com',
+                        subject: 'Extracted PDF',
+                        text: '',
+                        attachments: [
+                          {
+                            filename: attachment.filename,
+                            content: attachment.content
+                          }
+                        ]
+                      };
+                      transporter.sendMail(mailOptions, function(error, info) {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log('Email sent: ' + info.response);
+                          console.log('PDF attached without saving to the file system'); // Debugging line
                         }
-                      ]
-                    };
-                    transporter.sendMail(mailOptions, function(error, info) {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log('Email sent: ' + info.response);
-                        console.log('PDF attached without saving to the file system'); // Debugging line
-                      }
-                    });
-                  }
-                });
-              }
+                      });
+                    }
+                  });
+                }
+              });
             });
           });
         });
-      });
-      f.once('end', function() {
-        imap.end();
+        f.once('end', function() {
+          imap.end();
+        });
       });
     });
   });
-});
 
-imap.once('error', function(err) {
-  console.log(err);
-});
+  imap.once('error', function(err) {
+    console.log(err);
+  });
 
-imap.once('end', function() {
-  console.log('Connection ended');
-});
+  imap.once('end', function() {
+    console.log('Connection ended');
+  });
 
-imap.connect();
+  imap.connect();
+}
+
+// Schedule the checkEmails function to run every 5 minutes
+cron.schedule('*/5 * * * *', checkEmails);
 
